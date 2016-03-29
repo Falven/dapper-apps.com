b5b1964484bf0647875e0b722b67e9783978ca70
Francisco Aguilera <Falven2000@Hotmail.com>
2016-03-28 20:21:25 -0600

    Initial Project

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Dapper-Apps',
+    description: 'Dapper-Apps LLC Company Website.',
+    keywords: 'Dapper, Apps, Dapper-Apps, LLC'
+});});
+
+module.exports = router;
