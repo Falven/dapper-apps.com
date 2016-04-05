063a175518c1472c4c0dc2bd57a642c24cb173bf
Francisco Aguilera <Falven2000@Hotmail.com>
2016-04-05 00:01:14 -0600

    minor modifications to touch

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,16 @@
-var slider = document.getElementById("slider");
+if (document.readyState === "complete" || document.readyState === "interactive") {
+    init();
+} else {
+    document.addEventListener("DOMContentLoaded", init);
+}
 
-var items = document.getElementsByClassName("slider-items");
-
-slider.addEventListener("touchmove", function(event) {
-    if (event.targetTouches.length === 1) {
-        var touch = event.targetTouches[0];
-        items.style.left = touch.pageX + "px";
-    }
-}, false);
+function init(event) {
+    var slider = document.getElementById("slider");
+    var items = document.getElementsByClassName("slider-items");
+    slider.addEventListener("touchmove", function(event) {
+        if (event.targetTouches.length === 1) {
+            var touch = event.targetTouches[0];
+            items[0].style.transform = "translateX(" + touch.pageX + "px)";
+        }
+    }, false);
+}
--- views/js/index.js
+++ views/js/index.js
@@ -1,11 +1,20 @@
-var slider = document.getElementById('slider');
-var items = document.getElementsByClassName('slider-items');
-slider.addEventListener('touchmove', function(event) {
-  // If there's exactly one finger inside this element
-  if (event.targetTouches.length === 1) {
-    var touch = event.targetTouches[0];
-    // Place element where the finger is
-    items.style.left = touch.pageX + 'px';
-    // obj.style.top = touch.pageY + 'px';
-  }
-}, false);
+if (document.readyState === "complete" || document.readyState === "interactive") {
+  init();
+} else {
+  document.addEventListener("DOMContentLoaded", init);
+}
+
+function init(event) {
+  var slider = document.getElementById('slider');
+  var items = document.getElementsByClassName('slider-items');
+
+  slider.addEventListener('touchmove', function (event) {
+      // If there's exactly one finger inside this element
+      if (event.targetTouches.length === 1) {
+        var touch = event.targetTouches[0];
+        // Place element where the finger is
+        items[0].style.transform = 'translateX(' + touch.pageX + 'px)';
+        // obj.style.top = touch.pageY + 'px';
+      }
+    }, false);
+}
