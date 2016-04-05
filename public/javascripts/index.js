bb86796f165b5661346ac0ac83fd8587c26030ae
Francisco Aguilera <Falven2000@Hotmail.com>
2016-04-04 23:26:58 -0600

    added test slogans, playing with touch events

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,8 @@
-$(document).ready(function() {
-    console.log("ready!");
-    var MyBlah = function($blah) {
-        alert($blah);
-    };
-    MyBlah("Hello this works");
-});
+var obj = document.getElementsByClassName("slider-items");
+
+obj.addEventListener("touchmove", function(event) {
+    if (event.targetTouches.length === 1) {
+        var touch = event.targetTouches[0];
+        obj.style.left = touch.pageX + "px";
+    }
+}, false);
--- views/js/index.js
+++ views/js/index.js
@@ -1,10 +1,10 @@
-// index.js
-$(document).ready(function (){
-    console.log( "ready!" );
-
-    var MyBlah = function($blah) {
-        alert($blah);
-    };
-
-    MyBlah("Hello this works");
-});
+var obj = document.getElementsByClassName('slider-items');
+obj.addEventListener('touchmove', function(event) {
+  // If there's exactly one finger inside this element
+  if (event.targetTouches.length === 1) {
+    var touch = event.targetTouches[0];
+    // Place element where the finger is
+    obj.style.left = touch.pageX + 'px';
+    // obj.style.top = touch.pageY + 'px';
+  }
+}, false);
